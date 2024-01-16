3c5df14898ca24db9bcf0c97622c257246d7603c
Galan43 <jonatangalanh@gmail.com>
2024-01-15 21:18:44 -0600

    COMMIT 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,37 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ul>
-        <li>
-            <Link href ="/">
+    <div className="flex flex-col items-center">
+      <nav className="bg-gray-800 text-white p-4">
+        <ul className="text-2xl flex space-x-4">
+          <li className="my-2">
+            <Link href="/">
               Home
-            </Link> 
-        </li>
-        <li>
-            <Link  href="/pages/test">
+            </Link>
+          </li>
+          <li className="my-2">
+            <Link href="/pages/test">
               Test
             </Link>
-        </li>
+          </li>
         </ul>
-    </main>
-  )
+      </nav>
+      <div className="p-8">
+        <h2 className="text-2xl font-bold mb-4">Información del Alumno</h2>
+        <p>
+          <strong>Nombre:</strong> Jonatan Galan
+        </p>
+        <p>
+          <strong>Grupo:</strong>  TI02SM-22
+        </p>
+        <p>
+          <strong>Edad:</strong> 20 años
+        </p>
+        <p>
+          <strong>Curso:</strong> Ingeniería en software multiplataforma
+        </p>
+      </div>
+    </div>
+  );
 }
--- app/pages/test/page.tsx
+++ app/pages/test/page.tsx
@@ -1,5 +1,19 @@
-export default function Test(){
-    return <div>
-    Pagina de prueba
-    </div>
-}
+export default function Test() {
+    const containerStyle = {
+      maxWidth: '600px',
+      margin: '0 auto',
+      padding: '20px',
+      textAlign: 'center',
+      borderRadius: '10px',
+      boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
+    };
+
+  
+    return (
+      <div style={containerStyle}>
+        <h1>Página de prueba</h1>
+        <p>Futura imagen</p>
+      </div>
+    );
+  }
+  
