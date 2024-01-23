58c8cf7c4d5eb6d8267764729dcbe042f54e2d31
Galan43 <jonatangalanh@gmail.com>
2024-01-23 12:52:29 -0600

    login y registro

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 import styles from '@/styles/styles.module.css';
 import Gif from '@/components/Gif';
 import Footer from '@/components/Footer';
+import Counter from '@/components/Counter'
+import Login from '@/components/Login';
+import IndexPage from '@/components/IndexPage';
 
 export default function Home() {
   return (
     <div className={styles.content}>
-      {/* Contenido principal */}
+      {/* Contenido principal
       <h1 className={styles.h1Pri}>Bienvenidos!!</h1>
       <div className={styles.gifContainer}>
         <Gif />
       </div>
 
 
-      {/* Footer */}
-      <Footer />
+      {/* Footer 
+      <Footer /> */}
+      {/* <Counter/> */}
+      <IndexPage/>
     </div>
   )
 }
